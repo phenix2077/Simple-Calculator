de594c38d8ec136b1fb38253dc062d64be19dd47
phenix2077 <elielsermy04@gmail.com>
2026-01-12 02:41:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 using namespace std;
 
+// declaring function's operation
+double addition(double a, double b);
+double subtraction(double a, double b);
+double multiplication(double a, double b);
+double division
+
 int main()
 {
   // The main program.
