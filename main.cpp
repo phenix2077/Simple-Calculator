bea1b40b3ec8ef3d0546eba1b35b05721939ba71
phenix2077 <elielsermy04@gmail.com>
2026-01-12 02:53:57 +0200

    Add arithmetic functions with division error handling
    Implemented basic arithmetic functions: addition, subtraction, multiplication, and division with error handling for division by zero.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,34 @@ using namespace std;
 double addition(double a, double b);
 double subtraction(double a, double b);
 double multiplication(double a, double b);
-double division
+double division(double a, double b);
 
 int main()
 {
   // The main program.
+
+  return 0;
+}
+
+double addition(double a, double b)
+{
+  return a + b;
+}
+double subtraction(double a, double b)
+{
+  return a - b;
+}
+double multiplication(double a, double b)
+{
+  return a * b;
+}
+double division(double a, double b)
+{
+  if(b != 0)
+    return a / b;
+  else
+  {
+    cout << "Error: Division by zero!" << endl;
+    return 1;    // return 1 is for division by zero (0).
+  }
 }
